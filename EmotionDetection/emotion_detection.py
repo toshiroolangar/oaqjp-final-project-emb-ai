e6f9134d89fcef788b6b2b408cd1e59678761665
Toshiro Olangar <toshi.olangar04@gmail.com>
2024-07-29 15:24:59 -0400

    tried to fix key error for emotionPredictions but failed

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,12 +8,13 @@ def emotion_detector(text_to_analyze):
     response = requests.post(url, json = inputjson, headers=header)
     json_response = json.loads(response.text)
     json_response_dict = json_response['emotionPredictions'][0]['emotion']
+    dominant_emotion, probability = max(json_response_dict.items(), key=operator.itemgetter(1))
     anger_score = json_response['emotionPredictions'][0]['emotion']['anger']
     disgust_score = json_response['emotionPredictions'][0]['emotion']['disgust']
     fear_score = json_response['emotionPredictions'][0]['emotion']['fear']
     joy_score = json_response['emotionPredictions'][0]['emotion']['joy']
     sadness_score = json_response['emotionPredictions'][0]['emotion']['sadness']
-    dominant_emotion, probability = max(json_response_dict.items(), key=operator.itemgetter(1))
+    
     if response.status_code == 400:
         return {'anger' : None,
                 'disgust' : None,
