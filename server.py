65f8dd3b140ee891b5b8d4e2d7484f52305c466d
Toshiro Olangar <toshi.olangar04@gmail.com>
2024-07-29 15:29:00 -0400

    Fixed server.py pylint issues, added some documentations for functions and methods

--- server.py
+++ server.py
@@ -1,23 +1,34 @@
+'''
+Creates a flask web application for emotion_detection
+'''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 @app.route("/emotionDetector")
 def sent_emotion():
+    '''
+    Returns the details for the evaluated statement
+    '''
     statement_to_eval = request.args.get("textToAnalyze")
     response = emotion_detector(statement_to_eval)
     if response['dominant_emotion'] is None:
-        return f"Invalid text! Please try again!"
+        return "Invalid text! Please try again!"
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
     joy = response['joy']
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    return f"For the given statement, the system response is 'anger': {anger}, \
+    'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}.\
+     The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
 def render_index_page():
+    '''
+    Render home page
+    '''
     return render_template("index.html")
 
 if __name__ == "__main__":
